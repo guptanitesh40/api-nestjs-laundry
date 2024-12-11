b5d1ecaf27e59c62cdc5693f83941c0bfe06ee87
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2024-12-11 10:52:35 +0530

    update admin create order status update and price list pdf download customer

--- src/modules/price/price.controller.ts
+++ src/modules/price/price.controller.ts
@@ -41,7 +41,7 @@ export class PriceController {
   }
 
   @Post('download-pdf')
-  @Roles(Role.SUPER_ADMIN, Role.SUB_ADMIN)
+  @Roles(Role.SUPER_ADMIN, Role.SUB_ADMIN, Role.CUSTOMER)
   async downloadPDF(): Promise<StreamableFile> {
     const pdfBuffer = await this.priceService.generatePriceListPDF();
 
