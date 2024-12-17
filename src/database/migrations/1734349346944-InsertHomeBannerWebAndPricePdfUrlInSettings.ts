a1a42d0e65b68dbf9b1e9ee070c33c87280b347b
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2024-12-17 11:04:45 +0530

    Change the row name home_banner_web to home_promotion_banner_website in the settings table

--- src/database/migrations/1734349346944-InsertHomeBannerWebAndPricePdfUrlInSettings.ts
+++ src/database/migrations/1734349346944-InsertHomeBannerWebAndPricePdfUrlInSettings.ts
@@ -6,7 +6,7 @@ export class InsertHomeBannerWebAndPricePdfUrlInSettings1734349346944
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `INSERT INTO settings (setting_key, setting_value) 
-             VALUES ('home_banner_web', 
+             VALUES ('home_promotion_banner_website', 
              '{"title": "5 Saree Roll Press",
               "price": "200",
                "offer_validity": "20 Mar 2024",
@@ -21,7 +21,7 @@ export class InsertHomeBannerWebAndPricePdfUrlInSettings1734349346944
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM settings WHERE setting_key IN ('home_banner_web', 'price_pdf_url')`,
+      `DELETE FROM settings WHERE setting_key IN ('home_promotion_banner_website', 'price_pdf_url')`,
     );
   }
 }
