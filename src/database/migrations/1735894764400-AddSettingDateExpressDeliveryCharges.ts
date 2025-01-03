424288d13dab5b898d4dd7d9a1c6578d10f2cfd2
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-01-03 15:57:11 +0530

    change variable name express_delivery_charges to express_delivery_charge

--- src/database/migrations/1735894764400-AddSettingDateExpressDeliveryCharges.ts
+++ src/database/migrations/1735894764400-AddSettingDateExpressDeliveryCharges.ts
@@ -5,13 +5,13 @@ export class AddSettingDateExpressDeliveryCharges1735894764400
 {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
-        INSERT INTO settings (setting_key, setting_value) VALUES ('express_delivery_charges', '100')
+        INSERT INTO settings (setting_key, setting_value) VALUES ('express_delivery_charge', '100')
       `);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `DELETE FROM \`settings\` WHERE \`setting_key\` = 'express_delivery_charges'`,
+      `DELETE FROM \`settings\` WHERE \`setting_key\` = 'express_delivery_charge'`,
     );
   }
 }
