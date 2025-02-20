30491c668231619fdac4f54bdeb910caf1a879be
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-02-20 17:34:37 +0530

    update updateRolesFlag migrations

--- src/database/migrations/1740052688886-UpdateRolesFlag.ts
+++ src/database/migrations/1740052688886-UpdateRolesFlag.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpdateRolesFlag1740047318728 implements MigrationInterface {
+export class UpdateRolesFlag1740052688886 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       `UPDATE roles SET flag = TRUE WHERE role_id IN (2, 3, 6)`,
