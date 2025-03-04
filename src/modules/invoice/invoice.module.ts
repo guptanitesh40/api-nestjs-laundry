740902802f7ec83f74cd2191ee65ebb89f4373ec
tushar solanki <tusharsolanki@tushars-MacBook-Air.local>
2025-03-04 11:29:06 +0530

    genearte invoice to push notification app

--- src/modules/invoice/invoice.module.ts
+++ src/modules/invoice/invoice.module.ts
@@ -1,10 +1,12 @@
 import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from 'src/entities/order.entity';
+import { NotificationModule } from '../notification/notification.module';
 import { OrderModule } from '../order/order.module';
 import { PriceModule } from '../price/price.module';
 import { ProductModule } from '../products/product.module';
 import { ServicesModule } from '../services/services.module';
+import { UsersModule } from '../user/user.module';
 import { InvoiceController } from './invoice.controller';
 import { InvoiceService } from './invoice.service';
 
@@ -12,6 +14,8 @@ import { InvoiceService } from './invoice.service';
   imports: [
     TypeOrmModule.forFeature([Order]),
     forwardRef(() => OrderModule),
+    UsersModule,
+    NotificationModule,
     ProductModule,
     ServicesModule,
     PriceModule,
