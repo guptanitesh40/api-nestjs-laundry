37676a149eb509e2c14c44e87c31fcd9958bffa6
tushar-3154 <solankitushar2177@gmail.com>
2025-05-27 17:30:46 +0530

    remove unnecessary code

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -24,34 +24,6 @@ export class ReportService {
     private readonly razorpayRepository: Repository<RazorpayTransactions>,
   ) {}
 
-  // private formattedDateToSQL(dateStr: string): string {
-  //   const [day, month, year] = dateStr.split('-');
-  //   return `${year}-${month}-${day}`;
-  // }
-
-  // private convertDateParameters(
-  //   startDate?: string,
-  //   endDate?: string,
-  // ): { startDate: Date | undefined; endDate: Date | undefined } {
-  //   let start: Date | undefined;
-  //   let end: Date | undefined;
-
-  //   if (startDate) {
-  //     start = new Date(this.formattedDateToSQL(startDate));
-  //     start.setHours(0, 0, 0, 0);
-  //   }
-
-  //   if (endDate) {
-  //     end = new Date(this.formattedDateToSQL(endDate));
-  //     end.setHours(23, 59, 59, 999);
-  //   }
-
-  //   return {
-  //     startDate: start,
-  //     endDate: end,
-  //   };
-  // }
-
   private convertCountToNumber(arr) {
     return arr.map((c) => {
       c.count = Number(c.count);
